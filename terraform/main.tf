bfec6f74bed13cf1387275db7e41565922ce8221
ps185412 <98777839+ps185412@users.noreply.github.com>
2022-03-24 13:40:25 -0500

    add main

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+provider "azurerm" {
+  version = "=2.0.0"
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "ptsopstfstates-rg"
+    storage_account_name = "ptsopstf"
+    container_name       = "ptsopstfscontainer"
+    key                  = "terraform.tfstate"
+  }
+}
+
+resource "azurerm_resource_group" "rg-hello-azure" {
+  name     = "pts-appsre-test-rg"
+  location = "northcentralus"
+}
